4da781c2b103139a4035e04709ec9405bc2e1356
Alexei Shahov <93624312+direktor3092@users.noreply.github.com>
2022-10-18 01:31:29 +0300

    Delete code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,27 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(str, options) {
-  let string = String(str);
-  let repeatTimes = 1;
-  let separator = '+';
-  let addition = '';
-  let additionRepeatTimes = 1;
-  let additionSeparator = '|';
-  let prop = Object.entries(options);
 
-prop.forEach((value =>{
-  if(value[0] === 'repeatTimes') repeatTimes = value[1];
-  if(value[0] === 'separator') separator = value[1];
-  if(value[0] === 'addition') addition = String(value[1]);
-  if(value[0] === 'additionRepeatTimes') additionRepeatTimes = value[1];
-  if(value[0] === 'additionSeparator') additionSeparator = value[1];
-})
-
-let result = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
-return new Array(repeatTimes).fill(string + result).join(separator);
-
-}
 
 module.exports = {
   repeater
